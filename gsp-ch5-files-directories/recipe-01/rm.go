69015ec81f621678e8a66c6bd369033e0845399e
Ritesh Chauhan <chauhanx.ritesh@gmail.com>
2018-04-23 22:02:21 +0530

    adding options to rm command

--- gsp-ch5-files-directories/recipe-01/rm.go
+++ gsp-ch5-files-directories/recipe-01/rm.go
@@ -3,20 +3,40 @@ package main
 import (
 	"os"
 	"fmt"
+	"flag"
 )
 
+var (
+	VERBOSE = false
+)
+
+
+func verboseLog(msg string){
+	if VERBOSE{
+		fmt.Println(msg)
+	}
+}
+
 func main(){
-	arguments := os.Args
-	if len(arguments) == 1{
+
+	verbose := flag.Bool("v",false, "Verbose explains what the command is doing")
+	flag.Parse()
+	VERBOSE = *verbose
+
+	arguments := flag.Args()
+	if len(arguments) == 0{
 		fmt.Println("Please provide an argument")
 		os.Exit(1)
 	}
-	file := arguments[1]
-	err := os.Remove(file)
-	if err != nil{
-		fmt.Printf("%s\n", err.Error())
-		return
-	}else{
-		fmt.Println("successfully deleted file ", file)
+
+	for _,file := range arguments{
+		verboseLog(fmt.Sprintf("Deleting file %s", file))
+		err := os.Remove(file)
+		if err != nil{
+			verboseLog(fmt.Sprintf("%s\n", err.Error()))
+			return
+		}else{
+			verboseLog(fmt.Sprintf("successfully deleted file: %s\n", file))
+		}
 	}
 }
